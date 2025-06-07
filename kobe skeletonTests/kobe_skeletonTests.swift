d5317ab1090a1884b7fe22ef8623ea6e7ab13b07
abigailtov <abigail.t.stein@gmail.com>
2025-06-07 14:41:00 -0400

    files again

---
+++ kobe skeletonTests/kobe_skeletonTests.swift
@@ -0,0 +1,17 @@
+//
+//  kobe_skeletonTests.swift
+//  kobe skeletonTests
+//
+//  Created by Nate on 1/6/25.
+//
+
+import Testing
+@testable import kobe_skeleton
+
+struct kobe_skeletonTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
